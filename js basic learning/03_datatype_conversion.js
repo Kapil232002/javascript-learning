af5c2716a367376d2d63a66ba9095733cbeb960a
kapil kumar <kapilkumar232222@gmail.com>
2025-10-08 04:50:00 +0530

    datatype conversion

---
+++ js basic learning/03_datatype_conversion.js
@@ -0,0 +1,30 @@
+// marks ="23";
+// console.log(typeof marks);
+
+// marksInNumber = Number(marks);
+// console.log(typeof (marksInNumber)); //string to number conversion....
+
+// isMarried =1;
+// console.log(typeof isMarried)
+
+// boolean_isMarried = Boolean(isMarried)
+// console.log(boolean_isMarried)
+// console.log(typeof boolean_isMarried)
+
+
+/*
+0 = false
+1 =true
+"kapil" = true
+null = false
+"" = false
+*/
+
+
+number = 2;
+console.log(typeof number);
+
+string_conversion = String(number)
+console.log(typeof string_conversion);
+
+
